82481521189a8979a2500b505072ebb333cb9149
juancarlosbs <juanrov_@hotmail.com>
2021-05-07 15:53:59 -0300

    feat: preparing paths with getstaticpaths

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,9 +1,13 @@
-import NewMeetupForm from '../../components/meetups/NewMeetupForm'
 
-export default function NewMeetupPage() {
-    function addMeetupHandler(enteredMeetupData) {
-        console.log(enteredMeetupData)
-    }
+// our-domain.com/new-meetup
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
-    return <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+function NewMeetupPage() {
+  function addMeetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData);
+  }
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />
 }
+
+export default NewMeetupPage;
