0b21c568365b3f76be689470ebc5ab1e6b4b9231
juancarlosbs <juanrov_@hotmail.com>
2021-05-07 14:28:03 -0300

    feat: layout ready

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,9 @@
-import MeetupList from '../components/meetups/MeetupList';
+import NewMeetupForm from '../../components/meetups/NewMeetupForm'
+
+export default function NewMeetupPage() {
+    function addMeetupHandler(enteredMeetupData) {
+        console.log(enteredMeetupData)
+    }
+
+    return <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+}
